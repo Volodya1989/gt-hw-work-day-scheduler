fedaa526230e7a119758df2e08b3369cde6972f7
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-06-12 10:06:06 -0400

    refactoring code

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  var currentTime = moment();
+
   var hourId = [
     "nine",
     "ten",
@@ -32,32 +34,57 @@ $(document).ready(function () {
     "4PM",
     "5PM",
   ];
+
+  var hours = {};
   // adding current time
   moment().format();
   var d = moment().format("dddd, MMMM Do");
   var curTime = $(`<div class="time-block"><h5>${d}</h5></div`);
   $("#currentDay").append(curTime);
-  //adding rows and hours
+  //adding rows and sections
   for (var i = 0; i < 9; i++) {
-    var newRow = $(
-      `<div class='row'><div clas='hour col-sm-1' id =${hourId[i]}><h6>${hourValue[i]}</h6></div></div>`
-    );
+    hours[hourValue[i]] = moment();
+    var newRow = $(`
+      <div id=${hourId[i]} class='row'>
+        <span class='col-sm-1 hour'>${hourValue[i]}</span>
+        <textarea class='col-sm-10 past present future'></textarea>
+        <div class='col-sm-1 display-icon saveBtn btn'>
+        <i class='fas fa-lock'></i>
+      </div>
+      </div>
+    `);
     $(".container").append(newRow);
   }
-  //creating textarea
-  for (var i = 0; i < 1; i++) {
-    var textArea = $("<input>");
-    textArea.addClass("col-sm-8 past present future ");
-    $(".row").append(textArea);
-  }
-  //adding save button
-  for (var i = 0; i < 1; i++) {
-    var btnSave = $("<div><i class='fas fa-lock'></i></div>");
-    btnSave.addClass(" display-icon col-sm-1 saveBtn btn ");
-    $(".row").append(btnSave);
-  }
-
-  //   $('.display-icon').click(function(){
-  //     -----FONT AWESOME ICON------
-  //     });
 });
+// textArea.on("change", function () {});
+// btnSave.on("click", function () {});
+
+// version2=======================
+ // var textArea = $(
+    //   `<textarea class='col-sm-10 past present future'></textarea>`
+    // );
+    // var btnSave = $(`
+    //   <div class='col-sm-1 display-icon saveBtn btn'>
+    //     <i class='fas fa-lock'></i>
+    //   </div>
+    // `);
+    // $(".container").append(newRow);
+    // $("#" + hourId[i]).append(textArea, btnSave);
+
+// version1=======================
+//creating textarea
+// for (var i = 0; i < 1; i++) {
+//   var textArea = $("<input>");
+//   textArea.addClass("col-sm-10 past present future ");
+//   $(".row").append(textArea);
+// }
+// //adding save button
+// for (var i = 0; i < 1; i++) {
+//   var btnSave = $("<div><i class='fas fa-lock'></i></div>");
+//   btnSave.addClass("col-sm-1 display-icon  saveBtn btn ");
+//   $(".row").append(btnSave);
+// }
+
+//   $('.display-icon').click(function(){
+//     -----FONT AWESOME ICON------
+//     });
